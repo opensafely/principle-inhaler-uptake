944ec1498949e0463c753f614ab6e34f7190fff7
Jon Massey <jm4359@bristol.ac.uk>
2021-06-07 09:42:45 +0100

    initial study definition and code lists

---
+++ analysis/codelists.py
@@ -0,0 +1,7 @@
+from cohortextractor import codelist, codelist_from_csv
+
+covid_codelist = codelist_from_csv(
+    "codelists/opensafely-covid-identification-2020-06-03",
+    system="icd10",
+    column="icd10_code",
+)
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,13 +1,70 @@
 from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv
+from codelists import *
 
+'''
+Guidance issued 2021-04-12, with inclusion criteria of symptom onset within 14 days, therefore index date of 2021-03-29
+'''
 
+ix_dt = "2021-03-29"
 study = StudyDefinition(
     default_expectations={
         "date": {"earliest": "1900-01-01", "latest": "today"},
         "rate": "uniform",
         "incidence": 0.5,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+
+    index_date = ix_dt,
+
+    population=patients.satisfying(
+        
+        '''(age >= 65 OR ((age >=55 AND age <65)) AND has_comorbidities)) 
+        AND has_covid_pcr_positive
+        AND had_covid_symtoms
+        AND NOT corticosteroid_ADR
+        AND NOT has_covid_admission
+        AND NOT has_covid_emergency_admission'''
+        
     ),
+    age = patients.age_as_of("index_date"),
+    #stub
+    has_comorbidities = 1,
+    has_covid_pcr_positive = 1,
+    had_covid_symptoms = 1,
+
+    covid_admission_date=patients.admitted_to_hospital(
+    returning= "date_admitted",
+    with_these_diagnoses=covid_codelist,
+    on_or_after=ix_dt,
+    find_first_match_in_period=True,
+    date_format="YYYY-MM-DD",
+    return_expectations={"date": {"earliest": ix_dt}},
+
+    covid_emergency_admission_date=patients.attended_emergency_care(
+    returning= "date_admitted",
+    with_these_diagnoses=covid_codelist,
+    on_or_after=ix_dt,
+    find_first_match_in_period=True,
+    date_format="YYYY-MM-DD",
+    return_expectations={"date": {"earliest": ix_dt}},
+
+    # has_positive_covid_pcr = patients.with_test_result_in_sgss(
+
+    # )
+
+
+    first_positive_test_date=patients.with_test_result_in_sgss(
+    pathogen="SARS-CoV-2",
+    test_result="positive",
+    on_or_after=ix_dt,
+    find_first_match_in_period=True,
+    returning="date",
+    date_format="YYYY-MM-DD",
+    return_expectations={
+        "date": {"earliest" : ix_dt},
+        "rate" : "exponential_increase"
+    },
+),
+)
+)
+   
 )
