9361ee5cfff386a3fd8f0140dd63324e38f28011
Jon Massey <jon.massey@thedatalab.org>
2021-07-30 16:31:50 +0000

    correct test type exclusion

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -6,12 +6,14 @@ from datetime import date, timedelta
 Guidance issued 2021-04-12, with inclusion criteria of symptom onset within 14 days, therefore index date of 2021-03-29
 '''
 
-#todo: 
+#todo:
 
 ix_dt = "2021-03-29"
 
+
 def indexoffset(ndays):
-    return (date.fromisoformat(ix_dt) + timedelta(days = ndays)).strftime('%Y-%m-%d')
+    return (date.fromisoformat(ix_dt) + timedelta(days=ndays)).strftime('%Y-%m-%d')
+
 
 study = StudyDefinition(
     default_expectations={
@@ -20,10 +22,10 @@ study = StudyDefinition(
         "incidence": 0.5,
     },
 
-    index_date = ix_dt,
+    index_date=ix_dt,
 
     population=patients.satisfying(
-        
+
         # AND had_covid_symtoms
 
         """
@@ -37,28 +39,25 @@ study = StudyDefinition(
             AND NOT covid_emergency_admission
             AND NOT has_previous_steroid_prescription
             AND NOT primary_covid_hospital_admission
-            AND NOT first_positive_test_date = "LFT_Only"
+            AND first_positive_test_type != "LFT_Only"
         """,
 
         has_died=patients.died_from_any_cause(
-            on_or_before = "index_date",
-            returning = "binary_flag",
+            on_or_before="index_date",
+            returning="binary_flag",
         ),
 
-        has_previous_steroid_prescription = patients.with_these_medications(
+        has_previous_steroid_prescription=patients.with_these_medications(
             inhaled_or_systemic_corticosteroids,
-            on_or_before = "index_date - 90 days",
-            returning = "binary_flag",
-            return_expectations = {"incidence": 0.5}
+            on_or_before="index_date - 90 days",
+            returning="binary_flag",
+            return_expectations={"incidence": 0.5}
         ),
-        
-        registered = patients.registered_as_of("index_date"),
-    ),
-    
-   
 
-# would be nice to use "all tests" as this may exlude pts with initial +ve LFT followed by PCR
-     first_positive_test_date=patients.with_test_result_in_sgss(
+        registered=patients.registered_as_of("index_date"),
+    ),
+
+    first_positive_test_date=patients.with_test_result_in_sgss(
         pathogen="SARS-CoV-2",
         test_result="positive",
         on_or_after=ix_dt,
@@ -66,11 +65,11 @@ study = StudyDefinition(
         returning="date",
         date_format="YYYY-MM-DD",
         return_expectations={
-            "date": {"earliest" : ix_dt},
-            "rate" : "exponential_increase"
+            "date": {"earliest": ix_dt},
+            "rate": "exponential_increase"
         },
     ),
-    
+
     first_positive_test_type=patients.with_test_result_in_sgss(
         pathogen="SARS-CoV-2",
         test_result="positive",
@@ -78,26 +77,26 @@ study = StudyDefinition(
         find_first_match_in_period=True,
         returning="case_category",
         return_expectations={
-            "rate":"universal",
+            "rate": "universal",
             "category": {
                 "ratios": {
-                    "LFT_Only":0, 
-                    "PCR_Only":0.1, 
-                    "LFT_WithPCR":0.05,
-                    "":0.85
+                    "LFT_Only": 0,
+                    "PCR_Only": 0.1,
+                    "LFT_WithPCR": 0.05,
+                    "": 0.85
                 }
             }
         },
     ),
 
-    sex = patients.sex(
-        return_expectations = {
-        "rate": "universal",
-        "category": {"ratios": {"M": 0.49, "F": 0.51}},
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
         }
     ),
-    
-    imd = patients.categorised_as(
+
+    imd=patients.categorised_as(
         {
             "0": "DEFAULT",
             "1": """index_of_multiple_deprivation >=1 AND index_of_multiple_deprivation < 32844*1/5""",
@@ -106,12 +105,12 @@ study = StudyDefinition(
             "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
             "5": """index_of_multiple_deprivation >= 32844*4/5 """,
         },
-        index_of_multiple_deprivation = patients.address_as_of(
+        index_of_multiple_deprivation=patients.address_as_of(
             "index_date",
-            returning = "index_of_multiple_deprivation",
-            round_to_nearest = 100,
-            ),
-        return_expectations = {
+            returning="index_of_multiple_deprivation",
+            round_to_nearest=100,
+        ),
+        return_expectations={
             "rate": "universal",
             "category": {
                 "ratios": {
@@ -125,24 +124,24 @@ study = StudyDefinition(
             },
         },
     ),
-    age_band = patients.categorised_as(
+    age_band=patients.categorised_as(
         {
             "65_plus": "age >= 65",
             "55_65": "age >=55 AND age <65",
             "lt_55": "DEFAULT"
         },
-        age = patients.age_as_of("first_positive_test_date - 90 days"),
+        age=patients.age_as_of("first_positive_test_date - 90 days"),
         return_expectations={
-            "category":{
+            "category": {
                 "ratios": {
-                    "lt_55":0.85,
-                    "55_65":0.05,
-                    "65_plus":0.1,
-                    }
+                    "lt_55": 0.85,
+                    "55_65": 0.05,
+                    "65_plus": 0.1,
                 }
+            }
         }
     ),
-    region = patients.registered_practice_as_of(
+    region=patients.registered_practice_as_of(
         ix_dt,
         returning='nuts1_region_name',
         return_expectations={
@@ -180,33 +179,37 @@ study = StudyDefinition(
         }
     ),
 
-    budesonide_prescription = patients.with_these_medications(
-            budeonside_inhalers,
-            between = ["first_positive_test_date","first_positive_test_date + 14 days"],
-            returning = "number_of_matches_in_period",
-            return_expectations = {"int" : {"distribution": "normal", "mean": 1, "stddev": 1}, "incidence" : 0.5}
+    budesonide_prescription=patients.with_these_medications(
+        budeonside_inhalers,
+        between=["first_positive_test_date",
+                 "first_positive_test_date + 14 days"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 1, "stddev": 1}, "incidence": 0.5}
     ),
 
-    with_consultation = patients.with_gp_consultations(
-        between=["first_positive_test_date - 7 days","first_positive_test_date + 7 days"],
+    with_consultation=patients.with_gp_consultations(
+        between=["first_positive_test_date - 7 days",
+                 "first_positive_test_date + 7 days"],
         returning='binary_flag',
-        return_expectations = {"incidence": 0.5}
+        return_expectations={"incidence": 0.5}
     ),
 
-    post_budesonide_hospitalisation = patients.satisfying(
+    post_budesonide_hospitalisation=patients.satisfying(
         """
         budesonide_prescription>0 AND (
             covid_admission_date > budesonide_prescription_date OR 
             covid_emergency_admission_date > budesonide_prescription_date)
         """,
-        budesonide_prescription_date = patients.with_these_medications(
+        budesonide_prescription_date=patients.with_these_medications(
             budeonside_inhalers,
-            between = ["first_positive_test_date","first_positive_test_date + 14 days"],
-            returning = "date"
+            between=["first_positive_test_date",
+                     "first_positive_test_date + 14 days"],
+            returning="date"
         ),
 
         covid_admission_date=patients.admitted_to_hospital(
-            returning= "date_admitted",
+            returning="date_admitted",
             with_these_diagnoses=covid_codelist,
             on_or_after="first_positive_test_date",
             find_first_match_in_period=True,
@@ -214,16 +217,16 @@ study = StudyDefinition(
         ),
 
         covid_emergency_admission_date=patients.attended_emergency_care(
-            returning= "date_arrived",
+            returning="date_arrived",
             with_these_diagnoses=covid_codelist,
             on_or_after="first_positive_test_date",
             find_first_match_in_period=True,
             date_format="YYYY-MM-DD",
         ),
     ),
-    
+
     primary_covid_hospital_admission=patients.admitted_to_hospital(
-        returning= "binary_flag",
+        returning="binary_flag",
         with_these_primary_diagnoses=covid_codelist,
         on_or_after="first_positive_test_date",
         find_last_match_in_period=True,
@@ -233,28 +236,28 @@ study = StudyDefinition(
     ),
 
     covid_admission=patients.admitted_to_hospital(
-        returning= "binary_flag",
+        returning="binary_flag",
         with_these_diagnoses=covid_codelist,
         on_or_after="first_positive_test_date",
-        return_expectations = {"incidence": 0.05}),
+        return_expectations={"incidence": 0.05}),
 
     covid_emergency_admission=patients.attended_emergency_care(
-        returning= "binary_flag",
+        returning="binary_flag",
         with_these_diagnoses=covid_codes_ae,
         on_or_after="first_positive_test_date",
-        return_expectations = {"incidence": 0.05}),
-    
-    primis_shield = patients.with_these_clinical_events(
+        return_expectations={"incidence": 0.05}),
+
+    primis_shield=patients.with_these_clinical_events(
         primis_shield,
         returning='binary_flag',
         on_or_after='index_date - 1 year',
-        return_expectations = {"incidence": 0.1}
+        return_expectations={"incidence": 0.1}
     ),
 
-    primis_nonshield = patients.with_these_clinical_events(
+    primis_nonshield=patients.with_these_clinical_events(
         primis_nonshield,
         returning='binary_flag',
         on_or_after='index_date - 1 year',
-        return_expectations = {"incidence": 0.1}
+        return_expectations={"incidence": 0.1}
     )
 )
